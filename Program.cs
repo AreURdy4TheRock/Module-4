841b4bdb944ef50bd9c7c151109a4425a758f297
AreURdy4TheRock <dorkwingdock@gmail.com>
2023-02-15 17:32:57 +0300

    Практикум

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace Module_4
 {
@@ -6,33 +8,32 @@ namespace Module_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите свой любимый цвет на английском с маленькой буквы");
 
-            var color = Console.ReadLine();
-
-            switch (color)
+            (string Name, string LastName, string Login, int LoginLength, bool HasPet, double Age, string[] favcolors) [] User = new (string Name, string LastName, string Login, int LoginLength, bool HasPet, double Age, string[] favcolors) [3];
+            for (int k = 0; k < 3;k++) 
             {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Ваш любимый цвет красный");
-                    break;
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Ваш любимый цвет зелёный");
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("Ваш любимый цвет голубой");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Ваш любимый цвет жёлтый");
-                    break;
+                Console.WriteLine("Введите имя");
+                User[k].Name = Console.ReadLine();
+                Console.WriteLine("Введите Фамилию");
+                User[k].LastName = Console.ReadLine();
+                Console.WriteLine("Введите Логин");
+                User[k].Login = Console.ReadLine();
+                User[k].LoginLength = User[k].Login.Length;
+                Console.WriteLine("Есть ли у Вас животное? Да/Нет");
+                if (Console.ReadLine() == "Да")
+                {
+                    User[k].HasPet = true;
+                }
+                else User[k].HasPet = false;
+                Console.WriteLine("Введите Ваш возраст");
+                User[k].Age = double.Parse(Console.ReadLine());
+                User[k].favcolors = new string[3];
+                Console.WriteLine("Введите три любимых цвета пользователя");
+                for (int i = 0; i < User[k].favcolors.Length; i++)
+                {
+                    User[k].favcolors[i] = Console.ReadLine();
 
+                }
             }
             Console.ReadKey();
         }
